3df3e368ccb4c3fdc9148afec52c7122774200c5
Victor Bolta <victor.bolta@icloud.com>
2020-08-17 16:09:56 +0200

    Routes set

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :spaceships do
+    resources :bookings, only: [:show, :create]
+  end
+  resources :bookings, only: [:edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
