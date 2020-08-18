ef0716635683ee7604d44e01e37eef0c66560a8f
Victor Bolta <victor.bolta@icloud.com>
2020-08-18 15:50:58 +0200

    JS card

--- app/javascript/components/toggle_show_card.js
+++ app/javascript/components/toggle_show_card.js
@@ -4,10 +4,6 @@ const toggleShowCard = () => {
 
 var content = document.getElementById("content");
 
-// document.getElementById("circle-img").addEventListener('click', () => {
-//   content.classList.toggle("show");
-// });
-
 document.addEventListener('click', (event) => {
   if (event.currentTarget == document.getElementById("circle-img") || document.getElementById("content")) {
      content.classList.toggle("show")
