eec6bf537743a786977ebda117dd1ab7c6f64045
Farouk RAHMANI <rahmani.farouk@icloud.com>
2020-08-18 14:36:52 +0200

    pundit plus controller spaceships done

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,20 +1,52 @@
 class SpaceshipsController < ApplicationController
+  before_action :set_spaceship, only: [:show, :edit, :update, :destroy]
+
   def index
     @spaceships = Spaceship.all
+    @spaceships = policy_scope(Spaceship).order(created_at: :desc)
+  end
+
+  def show
+    authorize @spaceship
+  end
+
+   def edit
+    authorize @spaceship
   end
 
   def new
     @spaceship = Spaceship.new
+    authorize @spaceship
   end
 
   def create
     @spaceship = Spaceship.new(spaceship_params)
+    @spaceship.user = current_user
+    authorize @spaceship
+
     if @spaceship.save
-      redirect_to spaceship_path(@spaceship)
+      redirect_to spaceship_path(@spaceship), notice: 'Spaceship was successfully created.'
     else
       render :new
     end
   end
+
+  def update
+    authorize @spaceship
+    @spaceship.update(spaceship_params)
+  end
+
+  def destroy
+    authorize @spaceship
+    @spaceship.destroy
+    redirect_to spaceships_path
+  end
+
+  private
+
+    def set_spaceship
+      @spaceship = Spaceship.find(params[:id])
+    end
 
   def spaceship_params
     params.require(:spaceship).permit(:name, :year_of_creation, :country, :price, :description)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+victor = User.create!(name:"Victor", age:"26", email: "victor@gmail.com", password: "123456", admin: true)
+walid = User.create!(name:"Walid", age:"30", email: "walid@zoom.com", password: "micro-ouvert-a-vie", admin: false)
+segolene = User.create!(name:"Segolene", age:"36", email: "segolene@greenpeace.com", password: "wasteless", admin: false)
+nicolas = User.create!(name:"Nicolas", age:"32", email: "nicolas@parcdesprinces.fr", password: "Parisenfinaledimanche", admin: false)
+
+ariane = Spaceship.create!(name: "Ariane 5", year_of_creation: 1970, country: "Guyane Française", price: "100000$", description: "Une expérience unique dirigée par le CNES. Départ de la base de Kourou en Guyane Française", user_id: victor.id)
+apollo = Spaceship.create!(name: "ApolloXX", year_of_creation: 1960, country: "Etats-Unis", price: "10000000$", description: "Prêt au départ pour la Lune ? Départ de Cap Canaveral en Floride, sous la supervision de la NASA", user_id: segolene.id)
+space_x = Spaceship.create!(name: "StarShip", year_of_creation: 2015, country:"Etats-Unis", price: "100000000$", description: "Un aller simple pour Mars ?! Venez vous lancer sur la base de Space X en Floride avec Elon Musk", user_id: walid.id)
+soyouz = Spaceship.create!(name: "Soyouz", year_of_creation: 1958, country: "Russie", price: "1000$", description: "Départ à Baibounour au Kazakhstan. Venez faire votre ApéroBlindTest dans l'espace", user_id: nicolas.id)
+
