7d24fa9aac8cfc0f0e5f7544f43d1377a87f5752
victor bergeaud <victor.bergeaud@gmail.com>
2020-08-19 14:30:48 +0200

    show booking layout

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,46 +1,53 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [:update, :edit, :destroy]
+  before_action :set_booking, only: [:show, :update, :edit, :destroy]
   skip_after_action :verify_authorized, only: [:create]
 
-def create
-	@spaceship = Spaceship.find(params[:spaceship_id])
-	@booking = Booking.new(booking_params)
-	@booking.spaceship = @spaceship
-	@booking.user = current_user
+  def show
+    authorize @booking
+    set_booking
+    @spaceship = @booking.spaceship
+    @total_price = @booking.total_price
+  end 
 
-   #if @booking.departure_date && @booking.arrival_date
-    	#@booking.total_price = @booking.spaceship.price.to_f
- 	   #else
-    	#@booking.total_price = 0
-     #end
+  def create
+  	@spaceship = Spaceship.find(params[:spaceship_id])
+  	@booking = Booking.new(booking_params)
+  	@booking.spaceship = @spaceship
+  	@booking.user = current_user
 
-  	if @booking.save
-      redirect_to spaceship_path(@spaceship)
-  	else
-    	redirect_to spaceships_path
-  	end
-end
+     #if @booking.departure_date && @booking.arrival_date
+      	#@booking.total_price = @booking.spaceship.price.to_f
+   	   #else
+      	#@booking.total_price = 0
+       #end
+
+    if @booking.save
+       redirect_to spaceship_path(@spaceship)
+    else
+      	redirect_to spaceships_path
+    	end
+  end
 
   def update
-    redirect_to booking_path(@booking)
-  end
+      redirect_to booking_path(@booking)
+    end
 
   def edit
   end
 
-  def destroy
-    @booking.destroy
-    redirect_to spaceships_path
-  end
+    def destroy
+      @booking.destroy
+      redirect_to spaceships_path
+    end
 
-  private
+private
 
   def booking_params
-  	params.require(:booking).permit(:destination, :departure_date, :arrival_date, :total_price, :user_id)
+  params.require(:booking).permit(:destination, :departure_date, :arrival_date, :total_price, :user_id)
 
   end
 
   def set_booking
-  	@booking = Booking.find(params[:id])
+   @booking = Booking.find(params[:id])
   end
 end
